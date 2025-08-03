92e5a3bb7fa68d2375928e59fd03fc75b6bde0db
dwellerx <mr.steezyx@gmail.com>
2025-08-04 00:38:28 +0200

    Add full project with LFS for video files

--- src/Components/VideoContainer.jsx
+++ src/Components/VideoContainer.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const videoModules = require.context('../video', false, /\.(mp4|webm)$/);
-const videoPaths = videoModules.keys().map(videoModules);
+const videoLinks = [
+    'https://files.catbox.moe/pw595z.mp4',
+    'https://files.catbox.moe/2f38lz.mp4',
+    'https://files.catbox.moe/ic3hqk.mp4',
+    'https://files.catbox.moe/zkcmrl.mp4',
+    'https://files.catbox.moe/4d6g44.mp4',
+    'https://files.catbox.moe/x2ulpy.mp4',
+    'https://files.catbox.moe/cnpdty.mp4',
+    'https://files.catbox.moe/wv5f96.mp4',
+    'https://files.catbox.moe/y4rmb0.mp4',
+    'https://files.catbox.moe/50hr9m.mp4',
+    'https://files.catbox.moe/vcv7lp.mp4'
+];
 
 const VideoContainer = () => {
   return (
     <StyledWrapper>
-      <div style={{overflow: 'auto',scrollbarWidth: 'none'}} className="card">
+      <div className="card">
         <div className="label">VIDEOS</div>
         <div className="video-grid">
-          {videoPaths.map((src, i) => (
+          {videoLinks.map((src, i) => (
             <video key={i} src={src} controls muted loop className="video" />
           ))}
         </div>
@@ -26,34 +37,20 @@ const StyledWrapper = styled.div`
   height: 100vh;
 
   .card {
-    box-sizing: border-box;
     position: relative;
     width: 80dvh;
     height: 30dvh;
-    -ms-overflow-style: none; 
     top: 15dvh;
     background: rgba(255, 255, 255, 0.1);
     border: 1px solid rgba(255, 255, 255, 0.3);
     box-shadow: 0 8px 32px rgba(0, 0, 0, 0.25);
     backdrop-filter: blur(8px);
     border-radius: 2dvh;
-    overflow: hidden;
     padding: 1dvh;
     display: flex;
     flex-direction: column;
     gap: 1dvh;
-  }
-  .video-grid::-webkit-scrollbar {
-    display: none;        
-   } 
-  .label {
-    font-size: 1.5dvh;
-    color: white;
-    position: absolute;
-    bottom: 1dvh;
-    font-family: 'KaratinaRegular';
-    left: 1dvh;
-    z-index: 1;
+    overflow: hidden;
   }
 
   .video-grid {
@@ -66,6 +63,20 @@ const StyledWrapper = styled.div`
     scrollbar-width: none;
     justify-content: center;
     align-items: center;
+  }
+
+  .video-grid::-webkit-scrollbar {
+    display: none;
+  }
+
+  .label {
+    font-size: 1.5dvh;
+    color: white;
+    position: absolute;
+    bottom: 1dvh;
+    left: 1dvh;
+    font-family: 'KaratinaRegular';
+    z-index: 1;
   }
 
   .video {
